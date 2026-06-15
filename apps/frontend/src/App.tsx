0b2927dd224a60c78a7eb9094a95eeba2fccf4b8
isabellachoukaira <i.choukaira@gmail.com>
2026-06-14 22:06:14 -0300

    feat: criacao da pagina trilhas disponiveis

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { LoginPage } from "./pages/LoginPage";
+//import { LoginPage } from "./pages/LoginPage";
 //import { LessonPage } from "./pages/LessonPage";
+import { AvailableTracks } from "./pages/AvailableTracks";
 
 function App() {
-  return <LoginPage />;
+  return <AvailableTracks />;
 }
 
 export default App;
