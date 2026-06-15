6488bbdc9bcac9207ed5b2375b92929d4ab8ff9c
isabellachoukaira <i.choukaira@gmail.com>
2026-06-15 00:01:24 -0300

    feat: criacao da pagina de cadastro

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 //import { LoginPage } from "./pages/LoginPage";
 //import { LessonPage } from "./pages/LessonPage";
-import { AvailableTracks } from "./pages/AvailableTracks";
+//import { AvailableTracks } from "./pages/AvailableTracks";
+import { CadastroPage } from "./pages/cadastroPage";
 
 function App() {
-  return <AvailableTracks />;
+  return <CadastroPage />;
 }
 
 export default App;
